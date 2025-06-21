9072918e376529433413a1968234316079ccabeb
Harsh Prajapati <98733056+HarshPrajapati4926@users.noreply.github.com>
2025-06-21 21:34:53 +0530

    Update Sender.jsx

--- frontend/src/components/Sender.jsx
+++ frontend/src/components/Sender.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://video-streaming-platform-bf1p.onrender.com');
 
 export function Sender() {
   const videoRef = useRef(null);
