5d67364228f864fccadbc1be332e376c19b64bb4
Harsh Prajapati <98733056+HarshPrajapati4926@users.noreply.github.com>
2025-06-25 17:14:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -117,6 +117,6 @@ io.on('connection', socket => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Signaling server live on port ${PORT}`);
-});
+
 
 });
